6ab8a36ec6cf6026a2b399e745940c8f82143744
ASIK SK <asikeducation140@gmail.com>
2024-11-04 01:59:40 +0530

    1.4

--- pages/script.js
+++ pages/script.js
@@ -7,3 +7,23 @@ payBtn.addEventListener("click",()=>{
 closeBtn.addEventListener("click",()=>{
     form.classList.toggle("opne_form")
 })
+
+
+
+let url="https://script.google.com/macros/s/AKfycbyVVM83mxeUVyJZP-3833r2-i-4Vur8BSWCeQvkhh7RvW4n3Z32gd6k6z2gRZJo2oJYzQ/exec"
+
+let book_form=document.querySelector("#form");
+
+book_form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    let formDate=new FormData(book_form);
+    fetch(url,{
+        method:"POST",
+        body:formDate
+    }).then((res)=>{
+        res.text()
+    }).then((finalRes)=>{
+        console.log(finalRes);
+    })
+
+})
